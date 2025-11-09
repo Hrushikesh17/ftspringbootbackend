2fe7180c4514f6449fb3bedf52fead81a004d7fa
Hrushikesh17 <147137274+Hrushikesh17@users.noreply.github.com>
2025-11-09 19:37:19 +0530

    Disable CrossOrigin for SellerController
    Comment out CrossOrigin annotation for SellerController.

--- src/main/java/com/hkp/freetre/Controller/SellerController.java
+++ src/main/java/com/hkp/freetre/Controller/SellerController.java
@@ -23,7 +23,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/sellers")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
 public class SellerController {
 
 	 @Autowired
