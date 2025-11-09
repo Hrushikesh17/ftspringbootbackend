6510277be4e3fa7dbb3c590315347909bc68ddb2
Hrushikesh17 <hrushikeshpradhan055@gmail.com>
2025-11-09 15:48:23 +0530

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
-# Use a base image with JDK
-FROM openjdk:21-jdk-slim
-
-# Set the working directory
+# Step 1: Build the app (use Maven)
+FROM maven:3.9.4-eclipse-temurin-17 AS builder
 WORKDIR /app
+COPY . .
+RUN mvn -q -e -DskipTests package
 
-# Copy the packaged Spring Boot JAR file
-COPY target/freetre-0.0.1-SNAPSHOT.jar app.jar
+ENV PORT=10000
 
+# Step 2: Run the application
+FROM eclipse-temurin:17-jdk
+WORKDIR /app
+COPY --from=builder /app/target/*.jar app.jar
 
-# Expose the application's port 
-EXPOSE 8080
-
-# Command to run the application
-ENTRYPOINT ["java", "-jar", "app.jar"]
+EXPOSE 10000
+CMD ["java", "-jar", "app.jar"]
--- src/main/java/com/hkp/freetre/FreetreApplication.java
+++ src/main/java/com/hkp/freetre/FreetreApplication.java
@@ -3,17 +3,17 @@ package com.hkp.freetre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class FreetreApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+//        Dotenv dotenv = Dotenv.load();
+//        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//        System.setProperty("DB_USER", dotenv.get("DB_USER"));
+//        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 
         SpringApplication.run(FreetreApplication.class, args);
 
