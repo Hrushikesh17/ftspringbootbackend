fe23ec05d2dd93556ef761535a8efb1c0ef137d8
Hrushikesh17 <147137274+Hrushikesh17@users.noreply.github.com>
2025-11-09 19:37:56 +0530

    Update UserController.java

--- src/main/java/com/hkp/freetre/Controller/UserController.java
+++ src/main/java/com/hkp/freetre/Controller/UserController.java
@@ -26,7 +26,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
